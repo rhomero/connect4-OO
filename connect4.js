cdd63c22a360b45abab89d65a34b777bbdfb0f45
Homero Ramirez <homeroram@outlook.com>
2024-01-31 22:34:38 -0600

    change global var to instance

--- connect4.js
+++ connect4.js
@@ -5,14 +5,29 @@
  * board fills (tie)
  */
 
+class Game{
+  constructor(){
+    this.width = 7;
+    this.height = 6.
+    this.player = 1;
+    this.board = [];
+  }
+  makeBoard(){
+    for (let y = 0; y < HEIGHT; y++) {
+      board.push(Array.from({ length: WIDTH }));
+    }
+  }
+}
+
+/*
 const WIDTH = 7;
 const HEIGHT = 6;
 
 let currPlayer = 1; // active player: 1 or 2
 let board = []; // array of rows, each row is array of cells  (board[y][x])
 
-/** makeBoard: create in-JS board structure:
- *   board = array of rows, each row is array of cells  (board[y][x])
+ makeBoard: create in-JS board structure:
+ *   board = array of rows, each row is array of cells  (board[y][x]) 
  */
 
 function makeBoard() {
